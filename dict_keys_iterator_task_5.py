c1d1f4a0f83d04471726dcca9d164ee8332be8a2
Остап Грушецький <grushevskiy_o@icloud.com>
2023-08-30 21:56:44 +0300

    task 5

---
+++ dict_keys_iterator_task_5.py
@@ -0,0 +1,22 @@
+class DictKeysIterator:
+    def __init__(self, dictionary):
+        self.dictionary = dictionary
+        self.keys = list(self.dictionary.keys())
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index < len(self.keys):
+            key = self.keys[self.index]
+            self.index += 1
+            return key
+        else:
+            raise StopIteration
+
+
+user_dictionary = {"a": 1, "b": 2, "c": 3}
+dict_iter = DictKeysIterator(user_dictionary)
+for key in dict_iter:
+    print(key)
